ad7487fe95945dd34856b5a991b5fc1b524fc3a3
Nishantjangra <97723504+Nishantjangra@users.noreply.github.com>
2023-05-18 14:15:09 -0400

    shuu

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
-// setup a 'route' to listen on the default url path
+// setup a 'route' to listen on the default URL path
 app.get("/", (req, res) => {
-    res.send("Nishant Jangra - 150611200\n");
-    res.send("My favourite movie is Krish");
+    res.send("Nishant Jangra - 150611200\nMy favourite movie is Krish"); // Combine the two lines into one
 });
 
 // setup http server to listen on HTTP_PORT
