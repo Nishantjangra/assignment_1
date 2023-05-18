2a919ce9494c2e905a82545de17c29c4fdaf6af5
Nishantjangra <97723504+Nishantjangra@users.noreply.github.com>
2023-05-18 14:17:04 -0400

    jfeo;sirjfgheou;h

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default URL path
 app.get("/", (req, res) => {
-    res.send("Nishant Jangra - 150611200\nMy favourite movie is Krish"); // Combine the two lines into one
+    res.send("Nishant Jangra - 150611200<br>My favourite movie is Krish"); // Use <br> for a line break
 });
 
 // setup http server to listen on HTTP_PORT
